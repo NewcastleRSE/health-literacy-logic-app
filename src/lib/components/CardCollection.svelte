8c6ac91b30b71ea6156ffeae50c6172a4b610397
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-10-07 11:23:12 +0100

    finsihed styling landing page

--- src/lib/components/CardCollection.svelte
+++ src/lib/components/CardCollection.svelte
@@ -1,10 +1,14 @@
 <script>
-    import ProblemCard from '$lib/components/ProblemCard.svelte';
-    let {cardItems} = $props();
+	import ProblemCard from '$lib/components/ProblemCard.svelte';
+	let { cardItems } = $props();
 </script>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-    {#each cardItems as card}
-        <ProblemCard problem={card} />
-    {/each}
+<div
+	class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow-sm sm:grid sm:grid-cols-2 sm:divide-y-0 dark:divide-white/10 dark:bg-gray-900 dark:shadow-none dark:outline dark:-outline-offset-1 dark:outline-white/20"
+>
+	{#each cardItems as card}
+		<ProblemCard problem={card} />
+	{/each}
+	
 </div>
+
