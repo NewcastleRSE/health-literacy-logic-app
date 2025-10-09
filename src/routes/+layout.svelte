6e4b1f6ca1712a2315ba8190d731c4cc905486a1
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-10-09 08:37:16 +0100

    Correct links and paths to use {base} Fixes #9

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import { page } from '$app/state';
+	import {base} from '$app/paths';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -21,17 +22,17 @@
 				<div class="hidden sm:ml-6 sm:flex sm:space-x-8">
 					<!-- Current: "border-indigo-600 text-gray-900 dark:border-indigo-500 dark:text-white", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white" -->
 					<a
-						href="/"
+						href={base + "/"}
 						class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white"
 						>Home</a
 					>
 					<a
-						href="/#problems"
+						href={base + "/#problems"}
 						class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white"
 						>Problems</a
 					>
 					<a
-						href="/about"
+						href={base + "/about"}
 						class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:text-white"
 						>About</a
 					>
@@ -83,17 +84,17 @@
 		<div class="space-y-1 pt-2 pb-3">
 			<!-- Current: "bg-indigo-50 border-indigo-600 text-indigo-700 dark:border-indigo-500 dark:bg-indigo-600/10 dark:text-indigo-400", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white" -->
 			<a
-				href="/"
+				href={base + "/"}
 				class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
 				>Home</a
 			>
 			<a
-				href="/#problems"
+				href={base + "/#problems"}
 				class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
 				>Problems</a
 			>
 			<a
-				href="/about"
+				href={base + "/about"}
 				class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
 				>About</a
 			>
@@ -101,7 +102,7 @@
 	</el-disclosure>
 </nav>
 
-{#if page.url.pathname === '/'}
+{#if page.url.pathname === `${base}/`}
 	{@render children?.()}
 {:else}
 	<div id="problems" class="mx-auto mt-8 max-w-7xl px-6 pb-24 sm:mt-12 sm:pb-32 lg:px-8">
