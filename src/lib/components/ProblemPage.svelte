36fdf9cd6a91c814b1d4310a8f96d7eff3719c7d
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-10-09 13:34:05 +0100

    adds icon to json, adds icon to compoennet, adds icons

--- src/lib/components/ProblemPage.svelte
+++ src/lib/components/ProblemPage.svelte
@@ -10,6 +10,8 @@
 	class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800/50 dark:shadow-none dark:inset-ring dark:inset-ring-white/10"
 >
 	<div class="px-4 py-6 sm:px-6">
+		<div class="flex items-center justify-between">
+			<div>
 		<p class="mt-1 max-w-2xl text-sm/6 text-gray-500 dark:text-gray-300">
 			{problem['short-title']}
 		</p>
@@ -17,6 +19,17 @@
 		<span class="mb-2 inline-block rounded-full bg-blue-100 px-2 py-1 text-xs text-blue-800"
 			>{problem['level']}</span
 		>
+		</div>
+		{#if problem.icon}
+			<div>
+				<span
+					class="inline-flex rounded-lg bg-indigo-50 p-3 text-indigo-700 dark:bg-indigo-500/10 dark:text-indigo-400"
+				>
+					<img src="/assets/icons/{problem.icon}" alt="icon" width="64" height="64" />
+				</span>
+			</div>
+		{/if}
+		</div>
 	</div>
 	<div class="border-t border-gray-100 dark:border-white/5">
 		<dl class="divide-y divide-gray-100 dark:divide-white/5">
